a75f790882853d34bf56c506041e43a7f00bc29d
Manir Uddin <manir22sust@gmail.com>
2023-03-23 13:58:12 +0100

    hotels, rooms routes updated

--- controllers/room.js
+++ controllers/room.js
@@ -37,6 +37,22 @@ export const updateRoom = async (req, res, next) => {
     next(err);
   }
 };
+
+export const updateRoomAvailability = async (req, res, next) => {
+  try {
+    await Room.updateOne(
+      { "roomNumbers._id": req.params.id },
+      {
+        $push: {
+          "roomNumbers.$.unavailableDates": req.body.dates,
+        },
+      }
+    );
+    res.status(200).json(updateRoom);
+  } catch (err) {
+    next(err);
+  }
+};
 //DELETE
 export const deleteRoom = async (req, res, next) => {
   const hotelId = req.params.hotelid;
--- routes/hotels.js
+++ routes/hotels.js
@@ -1,9 +1,11 @@
 import express from "express";
 import {
   countByCity,
+  countByType,
   createHotel,
   deleteHotel,
   getHotel,
+  getHotelRooms,
   getHotels,
   updateHotel,
 } from "../controllers/hotel.js";
@@ -28,5 +30,6 @@ router.get("/find/:id", getHotel);
 router.get("/", getHotels);
 
 router.get("/countByCity", countByCity);
-router.get("/countByType", getHotels);
+router.get("/countByType", countByType);
+router.get("/room/:id", getHotelRooms);
 export default router;
